e385bd43391af7877d5ed298b334f71b218eef84
Luis Chavez <luischavezcald@gmail.com>
2019-10-06 13:56:46 -0400

    Initial setup

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const HelloWorld = () => (
+    <h1>Work it!</h1>
+)
+
+ReactDOM.render(HelloWorld(), document.getElementById('root'))
